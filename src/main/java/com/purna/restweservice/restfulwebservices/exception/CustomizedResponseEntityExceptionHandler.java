6896e9e85e24d54f0dca9949b40d99e4c75918ab
Purnasiva123 <purnasivaadari123@gmail.com>
2022-03-14 04:58:04 +0000

    Restful Project

---
+++ src/main/java/com/purna/restweservice/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.purna.restweservice.restfulwebservices.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.purna.restweservice.restfulwebservices.user.UserNotFoundException;
+
+@RestControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler 
+extends ResponseEntityExceptionHandler {
+	
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<Object> handleAllException
+		(Exception ex, WebRequest request){
+		
+		ExceptionResponse exceptionResponse = 
+				new ExceptionResponse(new Date(), ex.getMessage(), 
+						request.getDescription(false));
+		return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	@ExceptionHandler(UserNotFoundException.class)
+	public final ResponseEntity<Object> handleUserNotFoundException
+		(UserNotFoundException ex, WebRequest request){
+		
+		ExceptionResponse exceptionResponse = 
+				new ExceptionResponse(new Date(), ex.getMessage(), 
+						request.getDescription(false));
+		return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+	}
+}
+		
+
